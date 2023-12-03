64d7025c7e6c47569058073ebad867b753c8d633
Gergo Nagy <gnagy23@chemaxon.com>
2023-12-03 14:46:08 +0100

    [chatgpt]: refactor findNumber function

--- tasks/day3/index.ts
+++ tasks/day3/index.ts
@@ -5,35 +5,25 @@ let sum = 0;
 const input = readFileSync(`${__dirname}/input.txt`, "utf-8");
 const lines = input.split("\n");
 
-let isCorrect = false;
+enum Direction {
+    LEFT = "left",
+    RIGHT = "right",
+}
 
-const findLeftNumber = (line: string, index: number) => {
+const findNumber = (line: string, index: number, direction: Direction) => {
     let number = "";
 
-    for (let i = index - 1; i >= 0; i--) {
+    const increment = direction === Direction.RIGHT ? 1 : -1;
+    const start = direction === Direction.RIGHT ? index : index - 1;
+
+    for (let i = start; i >= 0 && i < line.length; i += increment) {
         const char = line[i];
 
         if (!Number.isInteger(+char)) {
             break;
         }
 
-        number = char + number;
-    }
-
-    return number;
-};
-
-const findRightNumber = (line: string, index: number) => {
-    let number = line[index];
-
-    for (let i = index + 1; i < line.length; i++) {
-        const char = line[i];
-
-        if (!Number.isInteger(+char)) {
-            break;
-        }
-
-        number = number + char;
+        number = direction === Direction.RIGHT ? number + char : char + number;
     }
 
     return number;
@@ -41,7 +31,6 @@ const findRightNumber = (line: string, index: number) => {
 
 for (let i = 0; i < lines.length; i++) {
     const line = lines[i];
-    let numbers: string[] = [];
 
     for (let j = 0; j < line.length; j++) {
         const numbers: string[] = [];
@@ -52,59 +41,87 @@ for (let i = 0; i < lines.length; i++) {
         }
 
         // Top left is number
-        if (lines[i - 1] && lines[i - 1][j - 1] && Number.isInteger(+lines[i - 1][j - 1])) {
-            const leftPart = findLeftNumber(lines[i - 1], j - 1);
-            const rightPart = findRightNumber(lines[i - 1], j - 1);
+        if (
+            lines[i - 1] &&
+            lines[i - 1][j - 1] &&
+            Number.isInteger(+lines[i - 1][j - 1])
+        ) {
+            const leftPart = findNumber(lines[i - 1], j - 1, Direction.LEFT);
+            const rightPart = findNumber(lines[i - 1], j - 1, Direction.RIGHT);
 
             numbers.push(leftPart + rightPart);
         }
 
         // Top is number
-        if (lines[i - 1] && lines[i - 1][j] && Number.isInteger(+lines[i - 1][j]) && !Number.isInteger(+lines[i - 1][j - 1])) {
-            const rightPart = findRightNumber(lines[i - 1], j);
+        if (
+            lines[i - 1] &&
+            lines[i - 1][j] &&
+            Number.isInteger(+lines[i - 1][j]) &&
+            !Number.isInteger(+lines[i - 1][j - 1])
+        ) {
+            const rightPart = findNumber(lines[i - 1], j, Direction.RIGHT);
 
             numbers.push(rightPart);
         }
 
         // Top right is number
-        if (lines[i - 1] && lines[i - 1][j + 1] && Number.isInteger(+lines[i - 1][j + 1]) && !Number.isInteger(+lines[i - 1][j])) {
-            const rightPart = findRightNumber(lines[i - 1], j + 1);
+        if (
+            lines[i - 1] &&
+            lines[i - 1][j + 1] &&
+            Number.isInteger(+lines[i - 1][j + 1]) &&
+            !Number.isInteger(+lines[i - 1][j])
+        ) {
+            const rightPart = findNumber(lines[i - 1], j + 1, Direction.RIGHT);
 
             numbers.push(rightPart);
         }
 
         // Left is number
         if (lines[i] && lines[i][j - 1] && Number.isInteger(+lines[i][j - 1])) {
-            const leftPart = findLeftNumber(lines[i], j);
+            const leftPart = findNumber(lines[i], j, Direction.LEFT);
 
             numbers.push(leftPart);
         }
 
         // Right is number
         if (lines[i] && lines[i][j + 1] && Number.isInteger(+lines[i][j + 1])) {
-            const rightPart = findRightNumber(lines[i], j + 1);
+            const rightPart = findNumber(lines[i], j + 1, Direction.RIGHT);
 
             numbers.push(rightPart);
         }
 
         // Bottom left is number
-        if (lines[i + 1] && lines[i + 1][j - 1] && Number.isInteger(+lines[i + 1][j - 1])) {
-            const leftPart = findLeftNumber(lines[i + 1], j - 1);
-            const rightPart = findRightNumber(lines[i + 1], j - 1);
+        if (
+            lines[i + 1] &&
+            lines[i + 1][j - 1] &&
+            Number.isInteger(+lines[i + 1][j - 1])
+        ) {
+            const leftPart = findNumber(lines[i + 1], j - 1, Direction.LEFT);
+            const rightPart = findNumber(lines[i + 1], j - 1, Direction.RIGHT);
 
             numbers.push(leftPart + rightPart);
         }
 
         // Bottom is number
-        if (lines[i + 1] && lines[i + 1][j] && Number.isInteger(+lines[i + 1][j]) && !Number.isInteger(+lines[i + 1][j - 1])) {
-            const rightPart = findRightNumber(lines[i + 1], j);
+        if (
+            lines[i + 1] &&
+            lines[i + 1][j] &&
+            Number.isInteger(+lines[i + 1][j]) &&
+            !Number.isInteger(+lines[i + 1][j - 1])
+        ) {
+            const rightPart = findNumber(lines[i + 1], j, Direction.RIGHT);
 
             numbers.push(rightPart);
         }
 
         // Bottom right is number
-        if (lines[i + 1] && lines[i + 1][j + 1] && Number.isInteger(+lines[i + 1][j + 1]) && !Number.isInteger(+lines[i + 1][j])) {
-            const rightPart = findRightNumber(lines[i + 1], j + 1);
+        if (
+            lines[i + 1] &&
+            lines[i + 1][j + 1] &&
+            Number.isInteger(+lines[i + 1][j + 1]) &&
+            !Number.isInteger(+lines[i + 1][j])
+        ) {
+            const rightPart = findNumber(lines[i + 1], j + 1, Direction.RIGHT);
 
             numbers.push(rightPart);
         }
@@ -118,3 +135,4 @@ for (let i = 0; i < lines.length; i++) {
 }
 
 console.log(sum);
+
