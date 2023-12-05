e6d28e4fa761dfef98b5af8bb9f32a1b5c271e9d
Gergo Nagy <gnagy23@chemaxon.com>
2023-12-05 06:02:39 +0100

    day4 part2 (removing part1)

--- tasks/day4/index.ts
+++ tasks/day4/index.ts
@@ -1,24 +1,51 @@
+import { readFileSync } from "fs";
 import { readStream } from "../../utils/readFileStream";
 
 let sum = 0;
 
-readStream(`${__dirname}/example.txt`, (line) => {
-    const games = line.split(":");
-    const [_,numbers] = games.map(game =>game.split('|'));
-    const winningNumbers = numbers[0].trimStart().split(' ').map(number => parseInt(number)).filter(Boolean);
-    const ourNumbers = numbers[1].trimStart().split(' ').map(number => parseInt(number)).filter(Boolean);
+const lineCount = readFileSync(`${__dirname}/input.txt`, "utf8").split(
+    "\n"
+).length;
+const scratchcards = new Array(lineCount).fill(1);
 
+readStream(`${__dirname}/input.txt`, (line) => {
+    const games = line.split(":");
+    const [game, numbers] = games.map((game) => game.split("|"));
+    const gameNumber = Number.parseInt(game[0].split(" ").filter(Boolean)[1]);
+    const winningNumbers = numbers[0]
+        .trimStart()
+        .split(" ")
+        .map((number) => parseInt(number))
+        .filter(Boolean);
+    const ourNumbers = numbers[1]
+        .trimStart()
+        .split(" ")
+        .map((number) => parseInt(number))
+        .filter(Boolean);
+
+    // Part1
     let counter = -1;
 
-    for(const number of ourNumbers){
-        if(winningNumbers.includes(number)){
-            counter++;
+    for (let i = 0; i < scratchcards[gameNumber - 1]; i++) {
+        counter = -1;
+        for (const number of ourNumbers) {
+            if (winningNumbers.includes(number)) {
+                counter++;
+            }
+        }
+
+        if (counter >= 0) {
+            for (
+                let i = gameNumber;
+                i <= counter + gameNumber && i < scratchcards.length;
+                i++
+            ) {
+                scratchcards[i]++;
+            }
         }
     }
 
-    if(counter >= 0){
-        sum += Math.pow(2, counter);
-    }
-
-    return sum;
+    // Part2
+    return scratchcards.reduce((a, b) => a + b, 0);
 });
+
