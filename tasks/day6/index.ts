e3b7df7fed6a2832615500f180fd0d78e7dbdd0f
Gergo Nagy <gnagy23@chemaxon.com>
2023-12-06 18:45:47 +0100

    day6 part2

--- tasks/day6/index.ts
+++ tasks/day6/index.ts
@@ -5,26 +5,27 @@ const input = readFileSync(`${__dirname}/example.txt`, "utf8");
 const times = input
     .split("\n")[0]
     .split(" ")
-    .filter((time) => Number.parseInt(time));
+    .filter((time) => Number.parseInt(time))
+    .join("");
 const distances = input
     .split("\n")[1]
     .split(" ")
-    .filter((time) => Number.parseInt(time));
+    .filter((time) => Number.parseInt(time))
+    .join("");
 
 const pairs: [number, number][] = [];
 
-for (let i = 0; i < times.length; i++) {
-    pairs.push([Number.parseInt(times[i]), Number.parseInt(distances[i])]);
-}
+pairs.push([Number.parseInt(times), Number.parseInt(distances)]);
 
 let prod = 1;
 
 pairs.forEach(([time, distance]) => {
+    console.log({ time, distance });
     const maxRangeCount = Math.floor(time / 2);
     for (let i = 0; i < maxRangeCount; i++) {
         if (i * (time - i) > distance) {
             console.log(i, time - i);
-            prod *= (time - i - i + 1)
+            prod *= time - i - i + 1;
             break;
         }
     }
